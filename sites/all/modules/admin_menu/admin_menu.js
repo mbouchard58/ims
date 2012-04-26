8648842b36de985f16b68e1fbd5fdb9cb5d7df77
Maury Bouchard <mbouchard58@yahoo.com>
2012-04-26 11:12:23 -0400

    after drush up 26 Apr 2012

--- sites/all/modules/admin_menu/admin_menu.js
+++ sites/all/modules/admin_menu/admin_menu.js
@@ -1,4 +1,3 @@
-/* $Id: admin_menu.js,v 1.32 2010/02/20 23:44:00 sun Exp $ */
 (function($) {
 
 Drupal.admin = Drupal.admin || {};
@@ -71,7 +70,7 @@ Drupal.behaviors.adminMenuCollapsePermissions = {
       // Freeze width of first column to prevent jumping.
       $('#permissions th:first', context).css({ width: $('#permissions th:first', context).width() });
       // Attach click handler.
-      $('#permissions tr:has(td.module)', context).once('admin-menu-tweak-permissions', function () {
+      $modules = $('#permissions tr:has(td.module)', context).once('admin-menu-tweak-permissions', function () {
         var $module = $(this);
         $module.bind('click.admin-menu', function () {
           // @todo Replace with .nextUntil() in jQuery 1.4.
@@ -83,7 +82,11 @@ Drupal.behaviors.adminMenuCollapsePermissions = {
             $row.toggleClass('element-hidden');
           });
         });
-      }).trigger('click.admin-menu');
+      });
+      // Get fragment from current URL.
+      var fragment = window.location.hash || '#';
+      // Collapse all but the targeted permission rows set.
+      $modules.not(':has(' + fragment + ')').trigger('click.admin-menu');
     }
   }
 };
@@ -126,7 +129,22 @@ Drupal.admin.getCache = function (hash, onSuccess) {
       Drupal.admin.hashes.hash = status;
     }
   });
-}
+};
+
+/**
+ * TableHeader callback to determine top viewport offset.
+ *
+ * @see toolbar.js
+ */
+Drupal.admin.height = function() {
+  var height = $('#admin-menu').outerHeight();
+  // In IE, Shadow filter adds some extra height, so we need to remove it from
+  // the returned height.
+  if ($('#admin-menu').css('filter') && $('#admin-menu').css('filter').match(/DXImageTransform\.Microsoft\.Shadow/)) {
+    height -= $('#admin-menu').get(0).filters.item("DXImageTransform.Microsoft.Shadow").strength;
+  }
+  return height;
+};
 
 /**
  * @defgroup admin_behaviors Administration behaviors.
@@ -173,7 +191,7 @@ Drupal.admin.behaviors.replacements = function (context, settings, $adminMenu) {
   for (var item in settings.admin_menu.replacements) {
     $(item, $adminMenu).html(settings.admin_menu.replacements[item]);
   }
-}
+};
 
 /**
  * Inject destination query strings for current page.
@@ -184,7 +202,7 @@ Drupal.admin.behaviors.destination = function (context, settings, $adminMenu) {
       this.search += (!this.search.length ? '?' : '&') + Drupal.settings.admin_menu.destination;
     });
   }
-}
+};
 
 /**
  * Apply JavaScript-based hovering behaviors.
--- sites/all/modules/admin_menu/admin_menu_toolbar/admin_menu_toolbar.js
+++ sites/all/modules/admin_menu/admin_menu_toolbar/admin_menu_toolbar.js
@@ -1,4 +1,3 @@
-/* $Id: admin_menu_toolbar.js,v 1.3 2010/02/19 23:19:00 sun Exp $ */
 (function($) {
 
 Drupal.admin = Drupal.admin || {};
@@ -16,7 +15,7 @@ Drupal.admin.behaviors = Drupal.admin.behaviors || {};
  */
 Drupal.admin.behaviors.toolbarActiveTrail = function (context, settings, $adminMenu) {
   if (settings.admin_menu.toolbar && settings.admin_menu.toolbar.activeTrail) {
-    $adminMenu.find('> div > ul > li > a[href=' + settings.admin_menu.toolbar.activeTrail + ']').addClass('active-trail');
+    $adminMenu.find('> div > ul > li > a[href="' + settings.admin_menu.toolbar.activeTrail + '"]').addClass('active-trail');
   }
 };
 
